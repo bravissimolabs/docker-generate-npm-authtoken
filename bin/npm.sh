bccf0908d23903703b00ac425c03e9b799131b95
Aleksandr Bakharev <aleksandr.bakharev@autodesk.com>
2016-04-13 16:51:56 +0200

    adding npm scopes support

--- bin/npm.sh
+++ bin/npm.sh
@@ -15,12 +15,17 @@ fi
 if [ -z "$NPM_REGISTRY" ]; then
     NPM_REGISTRY="registry.npmjs.org"
 fi
+if [ -z "$NPM_SCOPE" ]; then
+    NPM_SCOPE_OPTION=""
+else
+    NPM_SCOPE_OPTION="--scope=$NPM_SCOPE"
+fi
 
-/usr/local/bin/exp.sh "npm login --registry $NPM_REGISTRY" $NPM_USER $NPM_PASS $NPM_EMAIL >/dev/null 2>&1
+/usr/local/bin/exp.sh "npm login --registry $NPM_REGISTRY $NPM_SCOPE_OPTION" $NPM_USER $NPM_PASS $NPM_EMAIL >/dev/null 2>&1
 
 if [ ! -e ~/.npmrc ]; then
     echo "error logging into npm"
     exit 1
 else
-    echo $(cat ~/.npmrc)
+    echo $(cat ~/.npmrc) | sed 's/[[:blank:]]\+/\n/g'
 fi
